640f987f3eb15e9a13b031aca25f9b4e163f31d5
jstr-dev <adamcgl@outlook.com>
2024-08-17 02:40:38 +0100

    remove space

--- resources/js/components/Button.tsx
+++ resources/js/components/Button.tsx
@@ -6,7 +6,6 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function Button(ButtonProps: ButtonProps) {
-
     let ButtonStyles =
         "min-w-[100px] text-white bg-gray-800 hover:bg-gray-900 " +
         "focus:outline-none focus:ring-0.5 focus:ring-gray-300 "+
@@ -14,7 +13,7 @@ export default function Button(ButtonProps: ButtonProps) {
 
     return (
         <button onClick={ButtonProps.onClick} className={ ButtonStyles + " " + ButtonProps.className} type={ButtonProps.type}>
-            {ButtonProps.isLoading && 
+            {ButtonProps.isLoading &&
                 <Loader />
             }
 
