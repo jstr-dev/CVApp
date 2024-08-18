5a1abc389ae3734652054e0eb10c5fcb00e5444a
jemsnm <scildam1006@gmail.com>
2024-08-18 23:50:21 +0100

    remove unnecessary parameter

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -79,7 +79,7 @@ class UserTest extends TestCase
     /**
     * @dataProvider signup_invalid_credentials_provider
     */
-    public function test_signup_invalid_credentials($email, $password, $password_confirmation, $first_name, $last_name, $statusCode)
+    public function test_signup_invalid_credentials($email, $password, $password_confirmation, $first_name, $last_name)
     {
         $response = $this->post('/api/signup', [
             'email' => $email, 
@@ -89,18 +89,18 @@ class UserTest extends TestCase
             'last_name' => $last_name,
         ]);
 
-        $response->assertStatus($statusCode);
+        $response->assertStatus(400);
     }
 
     public static function signup_invalid_credentials_provider()
     {
         return [
-            ["test@gmail.com", "testPassword", "testPassword", "test", "test", 400], // email exists
-            ["test2@gmailcom", "test", "test", "test", "test", 400], // short password
-            ["test2@gmailcom", "invalidLongPassword", "invalidLongPassword", "test", "test", 400], // long password
-            ["test2@gmailcom", "testPassword", "testPassword", "t", "test", 400], // short first name
-            ["test2@gmailcom", "testPassword", "testPassword", "test", "t", 400], // short last name
-            ["test2@gmailcom", "mismatch", "testPassword", "test", "test", 400], //mismatch passwords
+            ["test@gmail.com", "testPassword", "testPassword", "test", "test"], // email exists
+            ["test2@gmailcom", "test", "test", "test", "test"], // short password
+            ["test2@gmailcom", "invalidLongPassword", "invalidLongPassword", "test", "test"], // long password
+            ["test2@gmailcom", "testPassword", "testPassword", "t", "test"], // short first name
+            ["test2@gmailcom", "testPassword", "testPassword", "test", "t"], // short last name
+            ["test2@gmailcom", "mismatch", "testPassword", "test", "test"], //mismatch passwords
         ];
     }
 }
