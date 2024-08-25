d79460b942a7fb813a31e91ca742a5d38a166b89
jemsnm <scildam1006@gmail.com>
2024-08-25 01:34:40 +0100

    add logout button to onboarding

--- resources/js/layouts/OnboardingLayout.tsx
+++ resources/js/layouts/OnboardingLayout.tsx
@@ -2,33 +2,39 @@ import React from 'react';
 import { getUserContext } from '../contexts/UserContext';
 import {StepperContainer, StepperItem} from '../components/Stepper';
 import Logo from '@/components/Logo';
+import Button from '@/components/Button';
 
 export default function OnboardingLayout({ children }: { children: React.ReactNode }) {
     const { user } = getUserContext();
 
     return (
         <div className="flex w-full h-full">
-            <div className="h-full flex flex-col gap-10 py-20 px-20 max-lg:hidden w-2/5">
-                <Logo />
+            <div className="flex flex-col gap-10 py-8 max-lg:hidden w-2/5 justify-between">
+                <div className='flex flex-col px-20 gap-20'>
+                    <Logo />
 
-                <StepperContainer>
-                    <StepperItem 
-                        title="Provide your details" 
-                        description="Enter your address" 
-                        icon="fa-address-book"
-                        active={user?.onboarding_stage === 'address'} 
-                        hasChild={true}
-                    />
+                    <StepperContainer>
+                        <StepperItem 
+                            title="Provide your details" 
+                            description="Enter your address" 
+                            icon="fa-address-book"
+                            active={user?.onboarding_stage === 'address'} 
+                            hasChild={true}
+                        />
 
-                    {/* TODO: This is temporary will merge in to the previous step */}
-                    <StepperItem 
-                        title="Enter your phone number" 
-                        description="Temporary text" 
-                        active={user?.onboarding_stage === 'mobile'} 
-                        hasParent={true}
-                        icon="fa-phone"
-                    />
-                </StepperContainer>
+                        {/* TODO: This is temporary will merge in to the previous step */}
+                        <StepperItem 
+                            title="Enter your phone number" 
+                            description="Temporary text" 
+                            active={user?.onboarding_stage === 'mobile'} 
+                            hasParent={true}
+                            icon="fa-phone"
+                        />
+                    </StepperContainer>
+                </div>
+                <div className='px-10'>
+                    <Button onClick={() => window.location.href = '/logout'} buttonStyle='secondary'>Logout</Button>
+                </div>
             </div>
 
             <div className="content-col flex flex-col justify-between items-center w-3/5 max-lg:w-full relative py-8">
