8451a75371308f27ee32a475a34abe6ccae09ecd
jemsnm <scildam1006@gmail.com>
2024-08-25 01:55:33 +0100

    no border on logout button

--- resources/js/layouts/OnboardingLayout.tsx
+++ resources/js/layouts/OnboardingLayout.tsx
@@ -33,7 +33,7 @@ export default function OnboardingLayout({ children }: { children: React.ReactNo
                     </StepperContainer>
                 </div>
                 <div className='px-10'>
-                    <Button onClick={() => window.location.href = '/logout'} buttonStyle='secondary' className="nobg">Logout</Button>
+                    <Button onClick={() => window.location.href = '/logout'} buttonStyle='secondary' className="nobg border-0">Logout</Button>
                 </div>
             </div>
 
