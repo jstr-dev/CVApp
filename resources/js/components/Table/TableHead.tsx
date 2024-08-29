7d6f480b50a64575b8378af6569f775f80d035e2
jemsnm <scildam1006@gmail.com>
2024-08-30 00:55:32 +0100

    update colours for dataheader

--- resources/js/components/Table/TableHead.tsx
+++ resources/js/components/Table/TableHead.tsx
@@ -6,10 +6,10 @@ interface TableHeadProps extends React.AllHTMLAttributes<HTMLDivElement> {
 
 function TableHead({ tableHeaders }: TableHeadProps) {
     return (
-        <div className="flex flex-col w-full bg-neutral-100 p-2 rounded-lg table-content-padding">
+        <div className="flex flex-col w-full background-col p-2 rounded-lg table-content-padding">
             <div className="flex flex-row justify-between">
                 {tableHeaders.map((header, index) => (
-                    <div key={index} style={{ flex: header.flex ?? 1 }} className="text-xs font-semibold">{header.header}</div>
+                    <div key={index} style={{ flex: header.flex ?? 1 }} className="text-xs font-semibold text-neutral-600">{header.header}</div>
                 ))}
             </div>
         </div>
