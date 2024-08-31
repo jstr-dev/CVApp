50b92d468ff10de95c9c5b9463feb6e6799bffff
jemsnm <scildam1006@gmail.com>
2024-08-31 23:33:31 +0100

    top navbar

--- resources/js/components/Navbar.tsx
+++ resources/js/components/Navbar.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { getNavContext } from "../contexts/NavContext";
 import { TAILWIND_BP } from '@/constants';
 import Logo from "./Logo";
+import Search from "./Search";
+import Button from "./Button";
 
 interface NavItemProps extends React.HTMLAttributes<HTMLDivElement> {
     name: string;
     icon: string;
     href?: string;
+    isOpen?: boolean;
 }
 
-function NavItem({ name, icon, href, className, onClick }: NavItemProps) {
+function NavItem({ name, icon, href, className, onClick, isOpen }: NavItemProps) {
     const { expanded } = getNavContext();
     const navigate = useNavigate();
     const isActive: boolean = href === window.location.pathname;
@@ -34,7 +37,7 @@ function NavItem({ name, icon, href, className, onClick }: NavItemProps) {
                 <i className={`text-lg text-gray-800 fa-solid ${icon} text-inherit`}></i>
             </div>
 
-            <div className={`flex-shrink-0 text-sm font-semibold leading-3 mb-[2px] ${!expanded && 'hide'}`}>{name}</div>
+            <div className={`flex-shrink-0 text-sm font-semibold leading-3 mb-[2px] ${expanded || !isOpen && 'hide'}`}>{name}</div>
         </div>
     )
 }
@@ -58,13 +61,65 @@ function NavSection({ title, children }: { title: string, children: React.ReactN
 function NavLogo()
 {
     return (
-        <div className="mb-5 nav-icon">
+        <div className={`${window.innerWidth < TAILWIND_BP.sm ? 'mb-3' : 'mb-5'} nav-icon`}>
             <Logo />
         </div>
     );
 }
 
-export default function Navbar() {
+function NavbarTop() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const modalpopup = () => {
+        alert('Coming Soon!')
+    }
+
+    return (
+        <div className="navtop w-full flexjustify-center">
+            <div className="w-full flex flex-row justify-between mt-5">
+                <NavLogo />
+
+                <div className="flex flex-row gap-1">
+                    <NavItem name="" className="mb-5" icon="fa-magnifying-glass" onClick={() => modalpopup()} />                           
+                    <NavItem name="" icon="fa-bell" href="/notifications" />
+                    <div className="relative">
+                        <NavItem name = "" icon="fa-bars" onClick={toggleMenu}/>
+                        {isOpen &&
+                        <div className={`absolute z-50 right-2 barmenu rounded-md shadow-md${isOpen ? 'block' : 'hide'}`}>
+                            <ul className="py-3 pl-2 pr-2 flex flex-col">
+                                <li>
+                                    <NavItem name="Dashboard" icon="fa-house" href="/" isOpen={isOpen} />
+                                </li>
+                                <li>
+                                    <NavItem name="Applications" icon="fa-check-to-slot" href="/applications" isOpen={isOpen} />  
+                                </li>
+                                <li>
+                                    <NavItem name="Resume Templates" icon="fa-file-alt" href="/templates" isOpen={isOpen} />
+                                </li>
+                                <li>
+                                    <NavItem name="Cover Letter Templates" icon="fa-envelope" href="/cover-letters" isOpen={isOpen} />
+                                </li>
+                                <li>
+                                    <NavItem name="Settings" icon="fa-gear" href="/settings" isOpen={isOpen} />
+                                </li>                                    
+                                <li>
+                                    <NavItem name="Logout" icon="fa-right-from-bracket" href="/logout" isOpen={isOpen} />
+                                </li>
+                            </ul>
+                        </div>
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function NavbarSide() {
     const { expanded, setExpanded } = getNavContext();
 
     const handleResize = () => {
@@ -129,3 +184,5 @@ export default function Navbar() {
         </div>
     )
 }
+
+export { NavbarSide, NavbarTop };
--- resources/js/layouts/MainLayout.tsx
+++ resources/js/layouts/MainLayout.tsx
@@ -1,12 +1,28 @@
-import Navbar from '@/components/Navbar';
-import React from 'react';
+import { NavbarSide, NavbarTop } from '@/components/Navbar';
+import { TAILWIND_BP } from '@/constants';
+import React, { useEffect, useState } from 'react';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="w-full h-full flex">
-            <Navbar />
+    const [isMobile, setIsMobile] = useState(window.innerWidth < TAILWIND_BP.sm);
+    
+    useEffect(() => {
+        const handleResize = () => {
+          setIsMobile(window.innerWidth < TAILWIND_BP.sm);
+        };
 
-            <div className='content w-full max-lg:pl-[80px] pl-[250px]'>
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+    
+    return (
+        <div className="w-full h-full flex flex-col">
+            {
+                isMobile ? <NavbarTop /> : <NavbarSide />
+            }
+            <div className={`content w-full ${isMobile ? 'pl-2' : 'max-lg:pl-[80px] pl-[250px]'}`}>
                 <div className="w-full h-full container p-6 mx-auto">
                     {children}
                 </div>
