7c2352c9b5d903a9004010c86bd7a323c795a21e
jemsnm <scildam1006@gmail.com>
2024-10-02 22:52:04 +0100

    tooltip

--- resources/js/components/Tooltip.tsx
+++ resources/js/components/Tooltip.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Tooltip({text} : {text: string}) {
     return (
-        <div className="absolute left-2 transform -translate-x-1/2 mb-2 hidden group-hover:block bg-gray-800 text-white text-sm p-2 rounded">
+        <div className="absolute text-xs left-2 transform -translate-x-1/2 mb-2 hidden group-hover:block bg-gray-700 text-white p-2 rounded-md">
             {text}
         </div>
     )
