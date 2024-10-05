83df78468b9e4a951b1d69bc929d176305d32dc1
jstr-dev <adamcgl@outlook.com>
2024-10-05 23:06:02 +0100

    remove odd delay

--- resources/js/components/TabMenu.tsx
+++ resources/js/components/TabMenu.tsx
@@ -19,10 +19,10 @@ function TabMenu({ tabs, ...props }: TabMenuProps) {
                 {tabs.map((tab, index) => (
                     <div
                         key={index}
-                        className={`relative text-sm font-semibold cursor-pointer px-1 ${currentTab === index ? 'text-blue-500' : 'text-gray-800'} transition-all ease-in-out delay-200`}
+                        className={`relative text-sm font-semibold cursor-pointer px-1 ${currentTab === index ? 'text-blue-500' : 'text-gray-800'}`}
                         onClick={() => setCurrentTab(index)}
                     >
-                        <div className={`${currentTab === index ? 'opacity-100' : 'opacity-0'} absolute bottom-[-10px] left-0 right-0 h-0.5 bg-blue-500 rounded-sm transition-all ease-in-out delay-200`}></div>
+                        <div className={`${currentTab === index ? 'opacity-100' : 'opacity-0'} absolute bottom-[-10px] left-0 right-0 h-0.5 bg-blue-500 rounded-sm`}></div>
                         {tab.name}
                     </div>
                 ))}
