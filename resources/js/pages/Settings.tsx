d3159acd264b3badd93ca441a5485627a6739054
jemsnm <scildam1006@gmail.com>
2025-03-07 21:49:38 +0000

    wip

--- resources/js/pages/Settings.tsx
+++ resources/js/pages/Settings.tsx
@@ -6,23 +6,36 @@ import TabMenu, { Tab } from '@/components/TabMenu';
 import Heading2 from '@/components/Heading2';
 
 const Tabs: Tab[] = [
-    { name: 'Account', content: <Account /> },
-    { name: 'General', content: <General /> },
-    { name: 'Privacy', content: <Privacy /> },
-]
+    { 
+        name: "Account", 
+        content: [<AccountPanel0 />, <AccountDetails />] 
+    },
+    { 
+        name: "General", 
+        content: [<GeneralPanel0 />] 
+    },
+    { 
+        name: "Privacy", 
+        content: [<PrivacyPanel0 />] 
+    }
+];
 
 function SettingsDetails() {
     return (          
         <> 
             <Heading className='w-2/3 mx-auto max-md:mx-0 max-md:w-full' title="Settings" />
-            <Panel className='w-2/3 h-full mx-auto max-md:w-full'>
+            <Panel className='w-2/3 h-full mx-auto max-md:w-full mb-6'>
                 <TabMenu tabs={Tabs} />
+            </Panel>
+
+            <Panel className='w-2/3 h-full mx-auto max-md:w-full'>
+                <h1>Settings</h1>
             </Panel>
         </>
     );
 }
 
-function Account() {
+function AccountPanel0() {
     const { user, setUserProperty } = getUserContext();
 
     return (
@@ -36,7 +49,15 @@ function Account() {
     );
 }
 
-function General() {
+function AccountDetails() {
+    return (
+        <div>
+            <h1>Account Details</h1>
+        </div>
+    );
+}
+
+function GeneralPanel0() {
     return (
         <div>
             <h1>General</h1>
@@ -44,7 +65,7 @@ function General() {
     );
 }
 
-function Privacy() {
+function PrivacyPanel0() {
     return (
         <div>
             <h1>Privacy</h1>
